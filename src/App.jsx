690aaa0fd96df6ad7cc147148e0b0068a194c914
themis1986 <themis_1986@hotmail.com>
2023-09-14 21:43:12 +0300

    Add error component, add loader function for fetching orders in Order.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
@@ -28,8 +31,10 @@ const router = createBrowserRouter([
         element: <CreateOrder />,
       },
       {
-        path: "/order/:oderId",
+        path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
